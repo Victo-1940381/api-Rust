80de994d6dc957abf38de6330c26a46a8a340aff
Victo-1940381 <1940381@carrefour.cegepvicto.ca>
2025-10-29 15:56:48 -0400

    test de la premiere route selement = success

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ async fn main() -> std::io::Result<()> {
                 .wrap(cors)
                 .wrap(Logger::default())
         })
-        .bind(("127.0.0.1", 8000))?
+        .bind(("127.0.0.1", 3000))?
         .run()
         .await
 }
